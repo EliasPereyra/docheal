24cb0347f32ec59c81dc3c3a7fb5bcf2bddd5a80
EliasPereyra <eliaspereyra_gomez@hotmail.com>
2024-08-09 09:52:57 -0300

    Create a custom use of json parse

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,14 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+/**
+ * Export a copy of the JSON.parse method for better reading
+ * @param user: unknown
+ * @returns
+ */
+export const stringifyValue = (value: unknown) =>
+  JSON.parse(JSON.stringify(value));
