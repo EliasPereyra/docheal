06e0c65234a584ba6c1d23e63205b0af65b51034
EliasPereyra <eliaspereyra_gomez@hotmail.com>
2024-08-13 08:48:03 -0300

    Add types for search params

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,5 +1,10 @@
 declare type Gender = "Hombre" | "Mujer" | "Otro";
 
+declare type SearchParamProps = {
+  params: { [key: string]: string };
+  searchParams: { [key: string]: string | string[] | undefined };
+};
+
 declare interface CreateUserParams {
   name: string;
   email: string;
