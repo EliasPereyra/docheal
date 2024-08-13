9352a10ce267b7537286b1103e767dba2eef4e75
EliasPereyra <eliaspereyra_gomez@hotmail.com>
2024-08-12 22:50:00 -0300

    Export db and the user collection

--- src/lib/appwrite.config.ts
+++ src/lib/appwrite.config.ts
@@ -8,3 +8,6 @@ client
   .setEndpoint(NEXT_PUBLIC_TD_ENDPOINT!)
   .setProject(TD_PROJECT_ID!)
   .setKey(TD_API_KEY!);
+
+export const databases = new appwriteSdk.Databases(client);
+export const users = new appwriteSdk.Users(client);
