bd1531fd2c0ffd6d3ee93524f793a698ca56cef5
EliasPereyra <eliaspereyra_gomez@hotmail.com>
2024-08-13 09:21:36 -0300

    Create two funcs for encrypting and decrypting keys

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -8,7 +8,22 @@ export function cn(...inputs: ClassValue[]) {
 /**
  * Export a copy of the JSON.parse method for better reading
  * @param user: unknown
- * @returns
+ * @returns any
  */
 export const stringifyValue = (value: unknown) =>
   JSON.parse(JSON.stringify(value));
+
+// TODO: reemplazar btoa con una función de seguridad mas robusta
+/**
+ * Una función que funciona para "encriptar" la clave, usando la función btoa.
+ *
+ * @param key
+ * @returns string
+ */
+export function encryptKey(key: string) {
+  return btoa(key);
+}
+
+export function decryptKey(key: string) {
+  return atob(key);
+}
