17e476a5e9f01af83ec0360340d98674169bbfc2
EliasPereyra <eliaspereyra_gomez@hotmail.com>
2024-08-19 23:00:15 -0300

    Add props to the input field

--- src/components/inputField.tsx
+++ src/components/inputField.tsx
@@ -1,19 +1,38 @@
+import { Control } from "react-hook-form";
+import { FormControl } from "./ui/form";
 import { Input } from "./ui/input";
+
+interface CustomProps {
+  control: Control<any>;
+}
 
 export function InputField({
   labelText,
   inputType,
+  name,
+  placeholder,
+  props,
+  image,
 }: {
   labelText: string;
   inputType?: string;
+  name: string;
+  placeholder?: string;
+  props?: CustomProps;
+  image?: string;
 }) {
   return (
-    <div className="flex flex-col gap-2">
-      <label>{labelText}</label>
-      <Input
-        type={inputType || "text"}
-        className="bg-[#181D30] border-[#2C3558]"
-      />
-    </div>
+    <FormControl>
+      <div className="flex flex-col gap-2">
+        <label>{labelText}</label>
+        <Input
+          type={inputType || "text"}
+          className="bg-[#181D30] border-[#2C3558]"
+          name={name}
+          placeholder={placeholder}
+          {...props}
+        />
+      </div>
+    </FormControl>
   );
 }
