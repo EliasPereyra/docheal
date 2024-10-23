b05eabd15ccfb847a4bd39b589a9617474238c1e
EliasPereyra <eliaspereyra_gomez@hotmail.com>
2024-10-23 10:14:11 -0300

    Add types for the appointment model

--- src/types/appwrite.types.ts
+++ src/types/appwrite.types.ts
@@ -3,3 +3,13 @@ import { Models } from "node-appwrite";
 export interface Patient extends Models.Document {
   fullname: string;
 }
+
+export interface Appointment extends Models.Document {
+  patient: Patient;
+  status: "pending" | "cancelled" | "completed";
+  reason: string;
+  additionalNotes: string;
+  appointmentDate: Date;
+  userId: string;
+  cancellationReason: string | null;
+}
