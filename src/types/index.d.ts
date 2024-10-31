ae176ff9397e5133a8e3f2b34a771b7562035815
EliasPereyra <eliaspereyra_gomez@hotmail.com>
2024-10-30 23:38:01 -0300

    --help

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -6,11 +6,36 @@ declare type SearchParamProps = {
 };
 
 declare interface CreateUserParams {
-  name: string;
+  fullName: string;
   email: string;
-  phone: string;
+  phoneNumber: string;
 }
 
 declare interface User extends CreateUserParams {
   $id: string;
 }
+
+declare interface RegisterPatientParams extends CreateUserParams {
+  fullName: string;
+  email: string;
+  phoneNumber: string;
+  birthDate: Date;
+  gender: Gender;
+  address: string;
+  profession: string;
+  civilStatus: string;
+  phoneNumberAlt: string;
+  healthInsuranceNumber: string;
+  allergies: string | undefined;
+  currentMedicines: string | undefined;
+  familyMedicalHistory: string | undefined;
+  pastFamilyMedicalHistory: string | undefined;
+  idType: string | undefined;
+  idNumber: string | undefined;
+  idPhotoUrl: FormData | undefined;
+  treatmentConsent: boolean;
+  disclosureConsent: boolean;
+  privacyConsent: boolean;
+}
+
+// TODO: create an interface for generating an appointment
