80f9b940c994e92eb3f2b4e498825bd8cebebfb7
EliasPereyra <eliaspereyra_gomez@hotmail.com>
2024-11-03 23:54:16 -0300

    Fix: add two missing props

--- src/types/appwrite.types.ts
+++ src/types/appwrite.types.ts
@@ -1,15 +1,15 @@
 import { Models } from "node-appwrite";
 
-export interface Patient extends Models.Document {
+export interface Patient {
   fullname: string;
 }
 
 export interface Appointment extends Models.Document {
   patient: Patient;
   status: "pending" | "cancelled" | "completed";
-  reason: string;
-  additionalNotes: string;
+  reason?: string;
+  additionalNotes?: string;
   appointmentDate: Date;
-  userId: string;
-  cancellationReason: string | null;
+  userId?: string;
+  cancellationReason?: string | null;
 }
