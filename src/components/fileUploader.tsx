ca736fb480ee5d86221c6a4e53c17a135cf35152
EliasPereyra <eliaspereyra_gomez@hotmail.com>
2024-11-05 23:14:08 -0300

    Adjust some styles to the file uploader component

--- src/components/fileUploader.tsx
+++ src/components/fileUploader.tsx
@@ -1,22 +1,28 @@
-import { convertFileToUrl } from "@/lib/utils";
 import Image from "next/image";
 import { useCallback } from "react";
 import { useDropzone } from "react-dropzone";
 
+import { convertFileToUrl } from "@/lib/utils";
+
 type FileUploaderProps = {
   files: File[] | undefined;
-  onChange: (file: File) => void;
+  onChange: (file: File[]) => void;
 };
 
 export function FileUploader({ files, onChange }: FileUploaderProps) {
-  const onDropHandler = useCallback((acceptedFiles: File[]) => {
-    onChange(acceptedFiles[0]);
+  const onDrop = useCallback((acceptedFiles: File[]) => {
+    onChange(acceptedFiles);
   }, []);
 
-  const { getRootProps, getInputProps } = useDropzone({ onDropHandler });
+  const { getRootProps, getInputProps } = useDropzone({
+    onDrop,
+  });
 
   return (
-    <div {...getRootProps()}>
+    <div
+      {...getRootProps()}
+      className="p-10 w-full text-center bg-[#181D30] hover:bg-[#1b1f35] border border-dashed border-[#2C3558] hover:border-[#47548a] transition-colors cursor-pointer"
+    >
       <input {...getInputProps()} />
       {files && files?.length > 0 ? (
         <Image
@@ -24,6 +30,7 @@ export function FileUploader({ files, onChange }: FileUploaderProps) {
           width={1000}
           height={1000}
           alt="Imagen del archivo subido"
+          className="max-h-[400px] overflow-hidden object-cover"
         />
       ) : (
         <>
@@ -32,13 +39,16 @@ export function FileUploader({ files, onChange }: FileUploaderProps) {
             width={40}
             height={40}
             alt="Imagen del icono de subida de archivos"
+            className="mx-auto"
           />
-          <div>
+          <div className="">
             <p className="">
               Haz click aquí para subir un archivo{" "}
               <span>o arrastra un archivo</span>
             </p>
-            <p>SVG, PNG, JPG (max. 800x400px)</p>
+            <small className="text-slate-500">
+              SVG, PNG, JPG (max. 800x400px)
+            </small>
           </div>
         </>
       )}
