d636949050352b36fdd5ae1e01c8acbd938e1ce3
EliasPereyra <eliaspereyra_gomez@hotmail.com>
2024-11-13 23:41:29 -0300

    Create a new fn for getting the latest appointments

--- src/lib/actions/appointment.actions.ts
+++ src/lib/actions/appointment.actions.ts
@@ -1,6 +1,6 @@
 "use server";
 
-import { ID } from "node-appwrite";
+import { ID, Query } from "node-appwrite";
 import { revalidatePath } from "next/cache";
 import {
   databases,
@@ -8,7 +8,13 @@ import {
   TD_DATABASE_ID,
 } from "../appwrite.config";
 import { parseStringify } from "../utils";
+import { Appointment } from "@/types/appwrite.types";
 
+/**
+ * Quest an appointment.
+ *
+ * @param appointment
+ */
 export const createAppointment = async (
   appointment: CreateAppointmentParams
 ) => {
@@ -24,5 +30,54 @@ export const createAppointment = async (
     return parseStringify(newAppointment);
   } catch (error) {
     console.error("Error creating appointment: ", error);
+  }
+};
+
+/**
+ * Obtiene las citas más recientes de forma descendente.
+ *
+ * @returns { scheduleCount, pendingCount, cancelledCount, total, documents }
+ */
+export const getLatestAppointments = async () => {
+  try {
+    const appointments = await databases.listDocuments(
+      TD_DATABASE_ID!,
+      TD_APPOINTMENT_COLLECTION_ID!,
+      [Query.orderDesc("$createdAt")]
+    );
+
+    const initialCounts = {
+      scheduledCount: 0,
+      pendingCount: 0,
+      cancelledCount: 0,
+    };
+
+    const counts = (appointments.documents as Appointment[]).reduce(
+      (acc, appointment) => {
+        switch (appointment.status) {
+          case "anotado":
+            acc.scheduledCount++;
+            break;
+          case "pendiente":
+            acc.pendingCount++;
+            break;
+          case "cancelado":
+            acc.cancelledCount++;
+            break;
+        }
+        return acc;
+      },
+      initialCounts
+    );
+
+    const data = {
+      ...counts,
+      total: appointments.total,
+      documents: appointments.documents,
+    };
+
+    return parseStringify(data);
+  } catch (error) {
+    console.error("Error when trying to get latest appointments: ", error);
   }
 };
