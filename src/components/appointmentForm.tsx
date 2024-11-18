b7d04bbc0f0f3a693c1644126ce964ef0ebd2766
EliasPereyra <eliaspereyra_gomez@hotmail.com>
2024-11-17 23:30:53 -0300

    Chore: add the logic for updating or cancelling an appointment

--- src/components/appointmentForm.tsx
+++ src/components/appointmentForm.tsx
@@ -2,7 +2,7 @@
 
 import { z } from "zod";
 import { useRouter } from "next/navigation";
-import { useEffect, useState } from "react";
+import { Dispatch, SetStateAction, useState } from "react";
 import { Form } from "./ui/form";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
@@ -10,42 +10,67 @@ import { toast } from "sonner";
 
 import CustomFormField, { FormFieldType } from "./CustomFormField";
 import { Button } from "./ui/button";
-import { CreateAppointFormSchemaValidation } from "@/lib/validation";
-import { createAppointment } from "@/lib/actions/appointment.actions";
+import { getAppointmentSchema } from "@/lib/validation";
+import {
+  createAppointment,
+  updateAppointment,
+} from "@/lib/actions/appointment.actions";
+import { Appointment } from "@/types/appwrite.types";
 
 export default function AppointmentForm({
   userId,
   patientId,
+  type = "crear",
+  appointment,
+  setOpen,
 }: {
   userId: string;
   patientId: string;
+  type: "crear" | "confirmar" | "cancelar";
+  appointment?: Appointment;
+  setOpen?: Dispatch<SetStateAction<boolean>>;
 }) {
   const [isLoading, setIsLoading] = useState(false);
   const router = useRouter();
-  const form = useForm<z.infer<typeof CreateAppointFormSchemaValidation>>({
-    resolver: zodResolver(CreateAppointFormSchemaValidation),
+  const AppointmentFormSchemaValidation = getAppointmentSchema(type);
+
+  const form = useForm<z.infer<typeof AppointmentFormSchemaValidation>>({
+    resolver: zodResolver(AppointmentFormSchemaValidation),
     defaultValues: {
-      reason: "",
-      additionalNotes: "",
-      appointmentDate: new Date(),
-      status: "pendiente",
+      reason: appointment ? appointment?.reason : "",
+      additionalNotes: appointment ? appointment?.additionalNotes : "",
+      appointmentDate: appointment
+        ? new Date(appointment?.appointmentDate)
+        : new Date(Date.now()),
+      cancellationReason: appointment?.cancellationReason || "",
     },
   });
 
   const onSubmitHandler = async (
-    values: z.infer<typeof CreateAppointFormSchemaValidation>
+    values: z.infer<typeof AppointmentFormSchemaValidation>
   ) => {
     setIsLoading(true);
 
+    let status;
+    switch (type) {
+      case "confirmar":
+        status = "anotado";
+        break;
+      case "cancelar":
+        status = "cancelado";
+        break;
+      default:
+        status = "pendiente";
+    }
+
     try {
-      if (patientId) {
+      if (type === "crear" && patientId) {
         const appointmentQuest = {
           patient: patientId,
-          reason: values.reason,
+          reason: values.reason!,
           additionalNotes: values.additionalNotes,
           appointmentDate: values.appointmentDate,
           userId: userId,
-          status: values.status,
         };
 
         const appointment = await createAppointment(appointmentQuest);
@@ -54,6 +79,27 @@ export default function AppointmentForm({
           setIsLoading(false);
           toast.success("Turno creado exitosamente");
           router.push(`/patients/${patientId}/create-appointment/success`);
+        }
+      } else {
+        const appointmentToUpdate = {
+          userId,
+          appointmentId: appointment?.$id!,
+          appointment: {
+            reason: values.reason!,
+            additionalNotes: values.additionalNotes,
+            appointmentDate: values.appointmentDate,
+            cancellationReason: values.cancellationReason,
+            status,
+          },
+          type,
+        };
+
+        const updatedAppointment = await updateAppointment(appointmentToUpdate);
+
+        if (updatedAppointment) {
+          setOpen && setOpen(false);
+          form.reset();
+          toast.success("Turno actualizado exitosamente");
         }
       }
     } catch (error) {
@@ -68,29 +114,53 @@ export default function AppointmentForm({
           className="flex flex-col gap-4"
           onSubmit={form.handleSubmit(onSubmitHandler)}
         >
-          <CustomFormField
-            control={form.control}
-            name="reason"
-            label="Razón de la consulta"
-            fieldType={FormFieldType.TEXTAREA}
-          />
+          {type === "cancelar" ? (
+            <CustomFormField
+              name="cancellationReason"
+              control={form.control}
+              label="Razón para cancelar el turno"
+              fieldType={FormFieldType.TEXTAREA}
+              placeholder="Ejemplo: Tuve un imprevisto y no pude asistir."
+            />
+          ) : (
+            <>
+              <CustomFormField
+                control={form.control}
+                name="reason"
+                label="Razón de la consulta"
+                placeholder="Ejemplo: Dolor de cabeza"
+                fieldType={FormFieldType.TEXTAREA}
+              />
 
-          <CustomFormField
-            control={form.control}
-            name="additionalNotes"
-            label="Comentarios/Notas adicionales"
-            fieldType={FormFieldType.TEXTAREA}
-          />
+              <CustomFormField
+                control={form.control}
+                name="additionalNotes"
+                label="Comentarios/Notas adicionales"
+                placeholder="Ejemplo: Tuve un dolor de cabeza desde ayer."
+                fieldType={FormFieldType.TEXTAREA}
+              />
 
-          <CustomFormField
-            control={form.control}
-            name="appointmentDate"
-            label="Fecha para el Turno"
-            fieldType={FormFieldType.DATE_PICKER}
-          />
+              <CustomFormField
+                control={form.control}
+                name="appointmentDate"
+                label="Fecha para el Turno"
+                fieldType={FormFieldType.DATE_PICKER}
+              />
+            </>
+          )}
 
-          <Button disabled={isLoading} className="bg-[#0C8EAF]" type="submit">
-            {isLoading ? "Cargando..." : "Solicitar Turno"}
+          <Button
+            disabled={isLoading}
+            className={`${type === "cancelar" && "bg-[#FF6C6C]"} bg-[#0C8EAF]`}
+            type="submit"
+          >
+            {isLoading
+              ? "Cargando..."
+              : type === "crear"
+              ? "Solicitar"
+              : type === "confirmar"
+              ? "Confirmar"
+              : "Cancelar"}
           </Button>
         </form>
       </Form>
