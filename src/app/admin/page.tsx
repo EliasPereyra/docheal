0bcd197b3b71afff6cb7cde0a88450365823add7
EliasPereyra <eliaspereyra_gomez@hotmail.com>
2024-11-17 23:34:16 -0300

    Use the datatable component and update the state cards

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,83 +1,50 @@
 import DataTable from "@/components/dataTable";
 import StateCard from "@/components/stateCard";
 import Topbar from "@/components/topbar";
+import { columns } from "@/components/columns";
+import { getLatestAppointments } from "@/lib/actions/appointment.actions";
 
-const patients = [
-  {
-    id: 1,
-    name: "Paciente 1",
-    date: "2022-01-01",
-    status: "Activo",
-    actions: "Ver",
-  },
-  {
-    id: 2,
-    name: "Paciente 2",
-    date: "2022-01-01",
-    status: "Activo",
-    actions: "Ver",
-  },
-];
+export default async function AdminPage() {
+  const latestAppointments = await getLatestAppointments();
 
-const headers = [
-  {
-    accessorKey: "Paciente",
-    header: "Paciente",
-  },
-  {
-    accessorKey: "Fecha",
-    header: "Fecha",
-  },
-  {
-    accessorKey: "Estado",
-    header: "Estado",
-  },
-  {
-    accessorKey: "Acciones",
-    header: "Acciones",
-  },
-];
-
-export default function AdminPage() {
   return (
     <section>
       <Topbar />
 
       <main className="flex flex-col gap-16 items-center p-16">
-        <div className="flex flex-col gap-2">
-          <h2 className="text-5xl font-bold">Bienvenido John Doe!</h2>
+        <div className="flex flex-col gap-2 items-start w-full">
+          <h2 className="text-5xl font-bold">
+            Bienvenido <span className="text-[#28C6EF]">John Doe!</span>
+          </h2>
           <p className="text-slate-400">
-            En esta sección necesitas agregar todos tus datos personales y
-            médicos.
+            En esta sección puedes ver todos los turnos de cada paciente y
+            administrarlos.
           </p>
         </div>
 
         {/* TODO: agregar efecto hover para las tarjetas */}
-        <div className="flex gap-4">
+        <div className="flex justify-between gap-4 w-full">
           <StateCard
-            count={78}
+            count={latestAppointments.scheduledCount}
             label="Total de turnos anotados"
             icon={"/assets/icons/calendar.svg"}
-            type="appointment"
+            type="scheduled"
           />
           <StateCard
-            count={15}
+            count={latestAppointments.pendingCount}
             label="Total de turnos pendientes"
             icon={"/assets/icons/time.svg"}
             type="pending"
           />
           <StateCard
-            count={9}
+            count={latestAppointments.cancelledCount}
             label="Total de turnos cancelados"
             icon={"/assets/icons/danger-triangle.svg"}
             type="cancelled"
           />
         </div>
 
-        {/* TODO: Agregar los estilos a la tabla y agregar datos mockeados */}
-        <div>
-          <DataTable columns={headers} data={patients} />
-        </div>
+        <DataTable columns={columns} data={latestAppointments.documents} />
       </main>
     </section>
   );
