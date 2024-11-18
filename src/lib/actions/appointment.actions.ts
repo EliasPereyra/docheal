062fe6d74a339756ec9b8e747f9ffaa4f928ffc2
EliasPereyra <eliaspereyra_gomez@hotmail.com>
2024-11-17 23:37:53 -0300

    Create a new service fn for updating an appointment

--- src/lib/actions/appointment.actions.ts
+++ src/lib/actions/appointment.actions.ts
@@ -87,16 +87,69 @@ export const getLatestAppointments = async () => {
 /**
  * For creating a sms message.
  *
- * @param content
  * @param userId
+ * @param content
  * @returns
  */
-export const sendSMSNotification = async (content: string, userId: string) => {
+export const sendSMSNotification = async (userId: string, content: string) => {
   try {
     const sms = await messaging.createSms(ID.unique(), content, [], [userId]);
 
     return parseStringify(sms);
   } catch (error) {
     console.error("There was an error when creating the sms: ", error);
+  }
+};
+
+/**
+ * Para actualizar un turno, se necesita el id del turno, el turno actualizado y el id del usuario.
+ * El ID del usuario es para mandarle una notificacion por sms.
+ * El tipo de turno es para decidir el tipo de mensaje de confirmacion que se envia.
+ *
+ * @param appointmentId
+ * @param newAppointment
+ * @param userId
+ * @param type
+ * @returns
+ */
+export const updateAppointment = async ({
+  appointmentId,
+  appointment,
+  userId,
+  type,
+}: UpdateAppointmentParams) => {
+  try {
+    const isAppointment = await databases.getDocument(
+      TD_DATABASE_ID!,
+      TD_APPOINTMENT_COLLECTION_ID!,
+      appointmentId
+    );
+
+    if (!isAppointment) {
+      throw new Error("The appointment does not exist");
+    }
+
+    const updatedAppointment = await databases.updateDocument(
+      TD_DATABASE_ID!,
+      TD_APPOINTMENT_COLLECTION_ID!,
+      appointmentId,
+      appointment
+    );
+
+    if (!updatedAppointment) throw Error;
+
+    const messageContent = `Saludos desde Docheal. ${
+      type === "confirmar"
+        ? `El turno ha sido confirmado para la fecha ${appointment.appointmentDate} - ${appointment.reason}`
+        : `Lamento informarle que su turno ha sido no ha sido confirmado. El motivo es ${appointment.cancellationReason}`
+    }`;
+
+    await sendSMSNotification(userId, messageContent);
+
+    revalidatePath("/admin");
+
+    return parseStringify(updatedAppointment);
+  } catch (error) {
+    console.error("There was an error when updating the appointment: ", error);
   }
 };
