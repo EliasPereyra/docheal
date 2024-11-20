8d00b99915d1c893cc8cdd15bc0e66173373a0b1
EliasPereyra <eliaspereyra_gomez@hotmail.com>
2024-11-20 12:04:23 -0300

    Improve the ui of the logo component

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,15 +1,15 @@
 import Image from "next/image";
 import Link from "next/link";
 
-export default function Logo() {
+export default function Logo({ w = 14 }: { w?: number }) {
   return (
     <Link href="/">
       <Image
         src="/assets/docheal-logo.png"
         alt="Logo oficial de la página"
-        width={206}
-        height={162}
-        className="w-20 md:w-14 mb-6"
+        width={20}
+        height={20}
+        className={`w-12 md:w-${w}`}
       />
     </Link>
   );
