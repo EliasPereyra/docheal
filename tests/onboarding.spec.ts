f6e1928951d29b980cf7dcb57c0aa1bd082e92b7
EliasPereyra <eliaspereyra_gomez@hotmail.com>
2025-01-02 15:12:13 -0300

    Add two more tests for the onboarding page

--- tests/onboarding.spec.ts
+++ tests/onboarding.spec.ts
@@ -21,8 +21,65 @@ test("Un usuario se puede registrar o loquear, y se redirige a la página de re
   await phone.fill("1234567890");
   await button.click();
 
-  await page.waitForTimeout(2000);
+  await page.waitForTimeout(3000);
   page.waitForLoadState("domcontentloaded");
 
   await expect(page).toHaveURL(/register/);
+});
+
+test("El admin puede iniciar sesión", async ({ page }) => {
+  await page.goto("/onboarding");
+
+  const adminLink = page.getByText("Admin");
+
+  await adminLink.click();
+
+  page.waitForLoadState("domcontentloaded");
+
+  expect(page).toHaveURL(/.*\/onboarding\?admin=true/);
+
+  const modal = page.getByRole("alertdialog");
+
+  await expect(modal).toBeVisible();
+
+  const otpNumber = page.locator("#otp");
+  const adminBtn = page.getByText(/Ingresar/);
+
+  const otp = "123456";
+
+  await otpNumber.fill(otp);
+  await adminBtn.click();
+
+  await page.waitForTimeout(5000);
+  page.waitForLoadState("domcontentloaded");
+
+  expect(page).toHaveURL(/admin/);
+});
+
+test("Muestra error al ingresar un OTP inválido", async ({ page }) => {
+  await page.goto("/onboarding");
+
+  const adminLink = page.getByText("Admin");
+
+  await adminLink.click();
+
+  page.waitForLoadState("domcontentloaded");
+
+  expect(page).toHaveURL(/.*\/onboarding\?admin=true/);
+
+  const modal = page.getByRole("alertdialog");
+
+  await expect(modal).toBeVisible();
+
+  const otpNumber = page.locator("#otp");
+  const adminBtn = page.getByText(/Ingresar/);
+
+  const otp = "111111";
+
+  await otpNumber.fill(otp);
+  await adminBtn.click();
+
+  await page.waitForLoadState("domcontentloaded");
+
+  expect(modal).toHaveText(/La clave es incorrecta/);
 });
