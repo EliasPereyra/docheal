f4850f3d972e59963aaa824b95fbdd49333f7e12
EliasPereyra <eliaspereyra_gomez@hotmail.com>
2025-01-16 22:56:20 -0300

    Ref: use the user context instead of passing the user data through props

--- src/app/patients/[userId]/register/page.tsx
+++ src/app/patients/[userId]/register/page.tsx
@@ -1,39 +1,24 @@
 import Image from "next/image";
-import { redirect } from "next/navigation";
 
 import { UserDataForm } from "@/components/CustomForms/userDataForm";
-import { getPatient, getUser } from "@/lib/actions/patient.actions";
-import Logo from "@/components/logo";
 import { Metadata } from "next";
-import { toast } from "sonner";
 
 export const metadata: Metadata = {
   title: "Registro | DocHeal Web App",
 };
 
-export default async function PatientForm({
-  params: { userId },
-}: SearchParamProps) {
-  const user = await getUser(userId);
-  const patient = await getPatient(userId);
-
-  if (patient) {
-    redirect(`/patients/${userId}/create-appointment`);
-  }
-
+export default async function PatientForm() {
   return (
     <section className="flex w-full">
       <div className="p-8 md:p-20 flex flex-col gap-10 w-full">
-        {/* NOTE Sección del texto principal  */}
-        <div className="flex flex-col gap-2">
-          <Logo w={100} h={100} className="mb-4" />
+        <div className="flex flex-col gap-4">
           <h2 className="text-4xl md:text-5xl font-bold">¡Bienvenido!</h2>
           <p className="text-slate-300">
             En esta sección necesitas agregar todos tus datos personales y
             médicos
           </p>
         </div>
-        <UserDataForm user={user} />
+        <UserDataForm />
       </div>
       <div className="">
         <Image
